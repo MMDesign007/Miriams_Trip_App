349041fbb80c3ac257a9ba7271d62fc14adc7253
Miriam Moore <miriamsusanmoore@gmail.com>
2015-03-07 16:55:28 -0600

    working on new trip form

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -17,3 +17,5 @@ post '/login' do
 end
 
 
+
+
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,3 +4,29 @@ get '/users/:user_id/trips' do
   @trips = @user.trips
   erb :index
 end
+
+get '/users/:user_id/trips/new' do 
+	@user = User.find(params[:user_id])
+	erb :'trips/new'
+end
+
+post '/users/:user_id/trips' do
+	#find by or create, finds the destination or creates a new destination
+	puts params
+	@destination = Destination.find_or_create_by(name: params[:destination])
+
+	#creates a new trip
+	@trip = Trip.new(destination: @destination, user_id: session[:user_id], days: params[:days])
+
+
+	if @trip.save
+		redirect "/users/#{params[:user_id]}/trips"
+	else
+		# @message = "please fill out!"
+		redirect "/users/#{params[:user_id]}/trips/new"
+		# erb :'trips/new'
+	end
+end
+
+
+
